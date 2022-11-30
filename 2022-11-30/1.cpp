0ea57b9098b8338dd6fd3b9707cceee6f6447f25
ion <utale.ion@gmail.com>
2022-11-30 19:06:11 +0000

    added test 1

---
+++ 2022-11-30/1.cpp
@@ -0,0 +1,38 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::istream;
+using std::ostream;
+
+/* 
+  run this in the terminal to test the code
+  g++ -o 1.exe  ./1.cpp&./1.exe
+
+  intput: 4578
+  result: 8644
+*/
+int main() {
+    // Write C++ code here
+    int x = 20;
+    std::cin >> x;
+
+    int c = 0;
+    int z = 0;
+    
+    do {
+        c = x % 10;
+        if (c % 2 != 0) {
+            z = z * 10 + c - 1;
+            std::cout << "if   :" << z << "\n";
+        } else {
+            z = z * 10 + c;
+            std::cout << "else :" << z << "\n";
+        }
+        x = x/10;
+    } while (x > 0 );
+    
+    std::cout << "final result:" << z << "\n"; 
+    return 0;
+}
+
